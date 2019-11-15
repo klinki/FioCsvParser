c467410c52f9bb556c30178903bd23d83e5037d1
David Klingenberg <dklingenberg@gmail.com>
2019-11-15 23:36:17 +0100

    Fix parsing code

--- FioCsvParser/Library.fs
+++ FioCsvParser/Library.fs
@@ -24,6 +24,8 @@ module Stocks =
         | DividendType of StockDividend
 
 module Fio =
+    open Stocks
+
     Encoding.RegisterProvider CodePagesEncodingProvider.Instance
 
     let [<Literal>] csvDefinitionFile = __SOURCE_DIRECTORY__ + "/Data/Obchody.csv"
@@ -72,48 +74,65 @@ module Fio =
 
         let groupByDividendType item =
             match item with
-                | DividendAmount | DividendTax | DividendFee _ -> true
+                | DividendAmount _ | DividendTax _ | DividendFee _ -> true
                 | _ -> false
 
         let filterOther item =
             match item with
-                | Other -> false
+                | Other _ -> false
                 | _ -> true
 
         let getFee (row: TradeRows.Row) =
-            match row.```Měna``` with
-                | "USD" -> decimal row.```Poplatky v USD```
-                | "CZK" -> decimal row.```Poplatky v CZK```
-                | "EUR" -> decimal row.```Poplatky v EUR```
+            match row.``Měna`` with
+                | "USD" -> decimal row.``Poplatky v USD``
+                | "CZK" -> decimal row.``Poplatky v CZK``
+                | "EUR" -> decimal row.``Poplatky v EUR``
+                | _     -> 0m
+
+        let getAmount (row: TradeRows.Row) =
+            match row.``Měna`` with
+                | "CZK" -> decimal row.``Objem v USD``
+                | "USD" -> decimal row.``Objem v CZK``
+                | "EUR" -> decimal row.``Objem v EUR``
                 | _     -> 0m
 
         let convertToBaseTrade (row: TradeRows.Row) =
             {
-                Date = row.``Datum obchodu``
+                Date = row.``Datum obchodu`` |> Option.defaultValue(new DateTime())
                 Ticker = row.Symbol
-                Count = row.```Počet```
-                Price = decimal row.```Cena```
+                Count = int row.``Počet``
+                Price = decimal row.``Cena``
                 Fee = getFee row
-                Total = decimal row.```Cena```
-                Currency = row.```Měna```
+                Total = decimal row.``Cena``
+                Currency = row.``Měna``
+            }
+
+        let convertToBaseTransfer (row: TradeRows.Row) =
+            {
+                Date = row.``Datum obchodu`` |> Option.defaultValue(new DateTime())
+                Amount = getAmount row
+                Currency = row.``Měna``
             }
 
         let convertToTrade item =
             let baseTrade = convertToBaseTrade item
-            let baseTradeWithTotal = { baseTrade with Total = (baseTrade.Price * baseTrade.Count) + baseTrade.Fee }
-            match item.```Směr``` with
+            let baseTradeWithTotal = { baseTrade with Total = (baseTrade.Price * decimal baseTrade.Count) + baseTrade.Fee }
+            match item.``Směr`` with
                 | Regex @"Nákup" [] -> Buy(baseTradeWithTotal)
                 | Regex @"Prodej" [] -> Sell(baseTradeWithTotal)
 
         let convertToTransfer item =
-
+            let baseTransfer = convertToBaseTransfer item
+            match item.``Text FIO`` with
+                | Regex @"na účet" [] -> Withdrawal(baseTransfer)
+                | Regex @"z účtu" [] -> Deposit(baseTransfer)
 
         let mapValidTypes item =
             match item with
-                | Trade row -> convertToTrade row
-                | Transfer row -> convertToTransfer row
+                | Trade row -> StockTradeType(convertToTrade row)
+                | Transfer row -> TransferType(convertToTransfer row)
 
-        let getDefaultDividend: StockDividend date symbol = {
+        let getDefaultDividend date symbol = {
             Date = date |> Option.defaultValue(new DateTime())
             Ticker = symbol
             Amount = 0m
@@ -124,18 +143,29 @@ module Fio =
 
         let mapToStockTypes groupedByDividends defaultDividend =
             match groupedByDividends with
-                    | (true, dividends) -> dividends |> Seq.fold reduceDivCollection defaultDividend
+                    | (true, dividends) -> dividends |> Seq.fold reduceDivCollection defaultDividend |> DividendType |> Seq.singleton
                     | (false, rest) -> rest |> Seq.filter filterOther
                                             |> Seq.map mapValidTypes
+                    | _ -> Seq.empty
 
-        let grouped = data.Rows |> Seq.groupBy (fun row -> (row.``Datum obchodu``, row.Symbol))
+        let grouped = data.Rows |> Seq.groupBy (fun (row: TradeRows.Row) -> (row.``Datum obchodu``, row.Symbol))
                                 |> Seq.map (fun tuple -> 
                                     let ((date, symbol), data) = tuple
                                     let mapped = data |> Seq.map mapRow 
                                     let result = mapped |> Seq.groupBy groupByDividendType
-                                                        |> Seq.map mapToStockTypes getDefaultDividend date symbol
-                                    result
+                                                        |> Seq.map (fun el -> mapToStockTypes el (getDefaultDividend date symbol))
+                                                        |> Seq.concat
+                                    result 
                                     )
+                                |> Seq.concat
+
+        for row in grouped do
+            match row with
+                | TransferType transfer -> printfn "Transfer: %A" transfer
+                | StockTradeType trade -> printfn "Trade %A" trade
+                | DividendType dividend -> printfn "Dividend %A" dividend
+                | _ -> printfn "Other"
+
 
         let classified = data.Rows |> Seq.map mapRow
         
